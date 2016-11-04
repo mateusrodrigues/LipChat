4dfca9ccb88245331332cd538a02b785405b1a07
Mateus Rodrigues de Morais <mateus.rodrigues@live.com>
2016-11-03 23:34:56 -0300

    Implement SignalR

---
+++ LipChat.Server/App_Start/Startup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(LipChat.Server.App_Start.Startup))]
+
+namespace LipChat.Server.App_Start
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
