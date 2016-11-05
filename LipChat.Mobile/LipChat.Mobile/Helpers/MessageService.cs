29e5485cfd7f75aaaf62a66bd7bea204f829923b
Mateus Rodrigues de Morais <mateus.rodrigues@live.com>
2016-11-05 16:57:25 -0200

    Implement SignalR on Xamarin

---
+++ LipChat.Mobile/LipChat.Mobile/Helpers/MessageService.cs
@@ -0,0 +1,57 @@
+﻿using LipChat.Library;
+using LipChat.Library.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LipChat.Mobile.Helpers
+{
+    public class MessageService
+    {
+        public static async Task<IEnumerable<Message>> GetMessagesAsync(int last = 100)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(Constants.APIAddress);
+
+                var response = await client.GetAsync($"api/messages?last={last}");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var content = await response.Content.ReadAsStringAsync();
+                    var messages = JsonConvert.DeserializeObject<IEnumerable<Message>>(content);
+
+                    return messages;
+                }
+
+                return null;
+            }
+        }
+
+        public static async Task<Message> PostMessageAsync(string content)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(Constants.APIAddress);
+
+                var postMessage = JsonConvert.SerializeObject(new { Content = content });
+                var response = await client.PostAsync("api/messages", new StringContent(postMessage, Encoding.UTF8,
+                    "application/json"));
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var requestContent = await response.Content.ReadAsStringAsync();
+                    var message = JsonConvert.DeserializeObject<Message>(requestContent);
+
+                    return message;
+                }
+
+                return null;
+            }
+        }
+    }
+}
--- LipChat.Mobile/LipChat.Mobile/MainPage.xaml.cs
+++ LipChat.Mobile/LipChat.Mobile/MainPage.xaml.cs
@@ -1,5 +1,10 @@
-﻿using System;
+﻿using LipChat.Library;
+using LipChat.Library.Models;
+using LipChat.Mobile.Helpers;
+using Microsoft.AspNet.SignalR.Client;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,9 +14,61 @@ namespace LipChat.Mobile
 {
     public partial class MainPage : ContentPage
     {
+        HubConnection _connection;
+        IHubProxy _hub;
+
+        public ObservableCollection<Message> Messages { get; set; }
+
         public MainPage()
         {
             InitializeComponent();
+            Messages = new ObservableCollection<Message>();
+
+            messagesListView.ItemsSource = Messages;
+
+            _connection = new HubConnection(Constants.APIAddress);
+            _hub = _connection.CreateHubProxy("ChatHub");
+            Task.Factory.StartNew(() => _connection.Start().Wait());
         }
+
+        private async void ContentPage_Appearing(object sender, EventArgs e)
+        {
+            var messages = await MessageService.GetMessagesAsync();
+
+            foreach (var m in messages.ToList())
+            {
+                Messages.Add(m);
+            }
+
+            Action<string> receiveMessage = (message) =>
+            {
+                var newMessage = new Message { MessageID = Guid.NewGuid(), Content = message };
+                Device.BeginInvokeOnMainThread(() =>
+                {
+                    AddMessageToList(newMessage);
+                });
+            };
+
+            _hub.On("receive", receiveMessage);
+        }
+
+        private async void btnSend_Clicked(object sender, EventArgs e)
+        {
+            if (tbMessage.Text.Length > 0)
+            {
+                tbMessage.IsEnabled = false;
+                await MessageService.PostMessageAsync(tbMessage.Text);
+                await _hub.Invoke("Send", tbMessage.Text);
+                tbMessage.IsEnabled = true;
+                tbMessage.Text = string.Empty;
+            }
+        }
+
+        #region Helpers
+        private void AddMessageToList(Message message)
+        {
+            Messages.Add(message);
+        }
+        #endregion
     }
 }
